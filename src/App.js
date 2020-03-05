8fa8303c4e4a8189020d95f24a4844027d274aeb
Paul Evans <paulevans89@gmail.com>
2020-03-05 20:16:32 +0000

    added button icon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import add from './add.svg'
 
 function App(props) {
     return (
@@ -9,30 +10,32 @@ function App(props) {
             <div id="input-container">
                 <input
                     id="text-input"
-                  autocomplete="off"
-                  type="text"
-                  placeholder="Type a reminder..."
-                  onChange={props.onChange}
-                  value={props.newItem}
+                    autocomplete="off"
+                    type="text"
+                    placeholder="Type a reminder..."
+                    onChange={props.onChange}
+                    value={props.newItem}
                 />
 
-                <button
-                  id="add-button"
-                  onClick={props.onClick}
-                ><img src="./add.svg" /></button>
+                <img 
+                    id="add-button"
+                    src={add}
+                    onClick={props.onClick}
+                />
             </div>
 
             {props.array.map(item => 
               <div id="item-container">
-                <li key={item.id}>
-                    <span id="bullet-point">.</span>
-                    <span id="list-text">{item.value}</span>
-                </li>
-                <button 
-                      id="remove-button"
-                      onClick={() => props.deleteItem(item.id)} 
-                ><div id="remove-symbol">+</div></button>
-                </div>
+                  <li key={item.id}>
+                      <span id="bullet-point">.</span>
+                      <span id="list-text">{item.value}</span>
+                  </li>
+                  <img 
+                        id="remove-button"
+                        src={add} 
+                        onClick={() => props.deleteItem(item.id)} 
+                  />
+              </div>
             )}
 
             {document.addEventListener("keyup", function(event) {
