d425f3342b0f60bf9428a15589de976380baa607
Paul Evans <paulevans89@gmail.com>
2020-03-05 19:41:23 +0000

    Updates

---
+++ precache-manifest.15be7f7f912a8b836ef37457a6a417ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b80f1356a32c0ad343d35c8509ecac",
+    "url": "/reminders-app/index.html"
+  },
+  {
+    "revision": "7ad014908709c5ad3731",
+    "url": "/reminders-app/static/css/main.f7ed3091.chunk.css"
+  },
+  {
+    "revision": "5f8bc3c5b64e69aa09cc",
+    "url": "/reminders-app/static/js/2.203eac9f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reminders-app/static/js/2.203eac9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ad014908709c5ad3731",
+    "url": "/reminders-app/static/js/main.fb5564a0.chunk.js"
+  },
+  {
+    "revision": "a0ef32d31888125403f3",
+    "url": "/reminders-app/static/js/runtime-main.f73650dc.js"
+  },
+  {
+    "revision": "03079f487621b4be7f32559d6ebc94ab",
+    "url": "/reminders-app/static/media/Averta-Regular.03079f48.otf"
+  },
+  {
+    "revision": "36153d464bc7b82f3b22dbccc739624f",
+    "url": "/reminders-app/static/media/TiemposHeadline-Semibold.36153d46.otf"
+  }
+]);
