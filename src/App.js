435e7f054d8c7ff59ab5e0b6004b396ef59d038c
Paul Evans <paulevans89@gmail.com>
2020-03-05 19:46:31 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    return (
+        <div>
+            <h1>Reminders<span class="heading-full-stop">.</span></h1>
+
+            <div id="input-container">
+                <input
+                    id="text-input"
+                  autocomplete="off"
+                  type="text"
+                  placeholder="Type a reminder..."
+                  onChange={props.onChange}
+                  value={props.newItem}
+                />
+
+                <button
+                  id="add-button"
+                  onClick={props.onClick}
+                ><img src="./add.svg" /></button>
+            </div>
+
+            {props.array.map(item => 
+              <div id="item-container">
+                <li key={item.id}>
+                    <span id="bullet-point">.</span>
+                    <span id="list-text">{item.value}</span>
+                </li>
+                <button 
+                      id="remove-button"
+                      onClick={() => props.deleteItem(item.id)} 
+                ><div id="remove-symbol">+</div></button>
+                </div>
+            )}
+
+            {document.addEventListener("keyup", function(event) {
+              // Number 13 is the "Enter" key on the keyboard
+                  if (event.keyCode === 13) {
+                    props.onClick();
+                  }
+              })
+            }
+        </div>
+    );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -4,7 +4,55 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Index extends React.Component {
+	componentDidMount() {
+		document.querySelector("#text-input").focus();
+	}
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			array: [],
+			newItem: ""
+		}
+		this.handleChange = this.handleChange.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+		this.deleteItem = this.deleteItem.bind(this);
+	}
+
+	handleChange(e) {
+		this.setState({ newItem: e.target.value });
+	}
+
+	handleClick() {
+		if(this.state.newItem) {
+			var newItemObject = {
+				id: 1 + Math.random(),
+				value: this.state.newItem
+			}
+			var arrayCopy = [...this.state.array];
+			arrayCopy.push(newItemObject);
+			this.setState({ 
+				array: arrayCopy,
+				newItem: ""
+			});
+			document.querySelector("#text-input").focus();
+		}
+	}
+
+	deleteItem(id) {
+		var arrayCopy = [...this.state.array];
+		var updatedArray = arrayCopy.filter(item => item.id !== id)
+		this.setState({ array: updatedArray });
+		document.querySelector("#text-input").focus();
+	}
+
+	render() {
+		return <App array={this.state.array} onChange={this.handleChange} onClick={this.handleClick} newItem={this.state.newItem} deleteItem={this.deleteItem}/>
+	}
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
